018d4afb186449b7a92553acb19ef36d4299b548
Vladislav Kovalskyi <v.darknesssb@gmail.com>
2020-09-05 22:19:41 +0300

    The file in which the tests are run

---
+++ test.py
@@ -0,0 +1,13 @@
+import asyncio
+
+from aioowm import OWM
+
+weather = OWM('85163474e6db3515b4fc39c3439c7220', 'ru')
+
+
+async def run():
+	result = await weather.get('Moscow')
+
+	print(result)
+
+asyncio.run(run())
