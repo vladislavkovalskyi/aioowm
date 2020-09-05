163afaa1d2c689d7539f8571c6e593ee4eaa7b45
Vladislav Kovalskyi <v.darknesssb@gmail.com>
2020-09-05 22:38:04 +0300

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+setuptools.setup(
+    name="aioowm",
+    version="1.0.0",
+    author="vladislavkovalskyi",
+    description="async OWM",
+    url="https://github.com/vladislavkovalskyi/aioowm",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        "aiohttp~=3.6.2",
+    ]
+)
