0fe545bb5059dd62f6fbf9bf458c7ddf50da609b
Vladislav Kovalskyi <v.darknesssb@gmail.com>
2020-09-05 22:12:21 +0300

    Delete app.py

---
+++ aioowm/__init__.py
@@ -0,0 +1,2 @@
+from .aioowm import OWM, Weather
+from .other import get
--- aioowm/aioowm.py
+++ aioowm/aioowm.py
@@ -1,9 +1,47 @@
 from typing import Optional
 
+import aiohttp
+
+from aioowm.other.const import const
+
 
 class OWM:
-	def __init__(self, token: Optional[str]):
+	def __init__(self, token: Optional[str], language: Optional[str]):
 		"""
 		:param token: your api-key https://home.openweathermap.org/api_keys
+		:param language: accepts country identifier (en, ru, fr)
 		"""
-		self.token = token
+		self.__token = token
+		self.__language = language
+
+	@property
+	async def token(self):
+		return self.__token
+
+	@token.setter
+	async def token(self, token: Optional[str]):
+		self.__token = token
+
+	@property
+	async def language(self):
+		return self.__language
+
+	@language.setter
+	async def language(self, language: Optional[str]):
+		self.__language = language
+
+
+class Weather(OWM):
+	def __init__(self, city: Optional[str]):
+		self.city = city
+
+	async def get(self):
+		async with aiohttp.ClientSession() as session:
+			async with session.get(
+					const.request_link.format(
+							city=self.city,
+							language=self.language,
+							token=self.token
+					)
+			) as response:
+				return await response.json(encoding='UTF-8')
