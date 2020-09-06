9ab892cad60eb9bc1fee9a21bb4353bcbba3ab7d
Vladislav Kovalskyi <v.darknesssb@gmail.com>
2020-09-06 13:10:04 +0300

    Rewritten HTTP-Client

--- aioowm/http/http.py
+++ aioowm/http/http.py
@@ -1,7 +1,9 @@
 from aiohttp import ClientSession
 
+from typing import Optional
 
-async def request(data):
+
+async def request(url: str, data: Optional[dict] = None) -> dict:
 	async with ClientSession() as session:
-		async with session.get(data) as response:
+		async with session.get(url=url, params=data) as response:
 			return await response.json(encoding='UTF-8')
