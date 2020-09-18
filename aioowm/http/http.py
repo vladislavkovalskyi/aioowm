7b07542a66105109468f7ad1bb4a6eec111f115d
Vladislav Kovalskyi <v.darknesssb@gmail.com>
2020-09-18 13:19:42 +0300

    Converted to double-string

--- aioowm/http/http.py
+++ aioowm/http/http.py
@@ -6,4 +6,4 @@ from typing import Optional
 async def request(url: str, data: Optional[dict] = None) -> dict:
 	async with ClientSession() as session:
 		async with session.get(url=url, params=data) as response:
-			return await response.json(encoding='UTF-8')
+			return await response.json(encoding="UTF-8")
