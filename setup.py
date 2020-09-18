63b4750a76ea509b57afc8fddfb9312dbc9c7eff
Vladislav Kovalskyi <v.darknesssb@gmail.com>
2020-09-18 13:36:36 +0300

    Updated setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,25 @@
 import setuptools
 
+try:
+    with open("README.md", "r", encoding="utf-8") as f:
+        long_description = f.read()
+except UnicodeDecodeError:
+    with open("README.md", "r") as f:
+        long_description = f.read()
+except FileNotFoundError:
+    long_description = ""
+
+with open("aioowm/const/const.py") as f:
+    exec(f.read())
+
 setuptools.setup(
 		name="aioowm",
-		version="1.0.1",
-		author="vladislavkovalskyi",
+		version=locals()["__version__"],
+		author=locals()["__author__"],
 		description="Async OpenWeatherMap API!",
-		url="https://github.com/vladislavkovalskyi/aioowm",
+		long_description=long_description,
 		license="MIT",
+		url="https://github.com/vladislavkovalskyi/aioowm",
 		packages=setuptools.find_packages(),
 		author_email="v.darknesssb@gmail.com",
 		classifiers=[
