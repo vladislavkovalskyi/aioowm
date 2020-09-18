bbb5eef4134c7d1d183c050b81fb5a7b12b204f3
Vladislav Kovalskyi <v.darknesssb@gmail.com>
2020-09-18 13:19:01 +0300

    Added new param "temperature" and updated get weather

--- aioowm/aioowm.py
+++ aioowm/aioowm.py
@@ -3,23 +3,33 @@ from typing import Optional
 from aioowm.const import const
 from aioowm.http.http import request
 from aioowm.types.models import Model
+from aioowm.handlers.handlers import language, temperature
 
 
 class OWM:
-	def __init__(self, token: str, language: Optional[str]):
+	def __init__(
+			self,
+			token: str,
+			language: Optional[str] = "en",
+			temperature: Optional[str] = "celsius"
+	):
 		"""
 		:param token: your api-key https://home.openweathermap.org/api_keys
 		:param language: accepts country identifier (en, ru, fr)
+		:param temperature: accepts temperature (celsius, fahrenheit, kelvin)
 		"""
+
 		self._token = token
 		self.language = language
+		self.temperature = temperature
 
 	async def get(self, city: str) -> Model:
 		response = await request(
 				const.weather_api, {
-					'q':     city,
-					'lang':  self.language,
-					'appid': self._token
+					"q": city,
+					"lang": await language(self.language),
+					"units": await temperature(self.temperature),
+					"appid": self._token
 				}
 		)
 		return Model(**response)
