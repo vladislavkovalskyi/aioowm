3bf46cff38032eaf745a492fc23ea77a737d065f
Vladislav Kovalskyi <v.darknesssb@gmail.com>
2020-09-18 13:21:19 +0300

    Updated setup.py

--- setup.py
+++ setup.py
@@ -1,19 +1,18 @@
 import setuptools
 
 setuptools.setup(
-    name="aioowm",
-    version="1.0.0",
-    author="vladislavkovalskyi",
-    description="async OWM",
-    url="https://github.com/vladislavkovalskyi/aioowm",
-    packages=setuptools.find_packages(),
-    classifiers=[
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Operating System :: OS Independent",
-    ],
-    install_requires=[
-        "aiohttp~=3.6.2",
-        "pydantic~=1.6.1"
-    ]
+		name="aioowm",
+		version="1.0.1",
+		author="vladislavkovalskyi",
+		description="Async OpenWeatherMap API!",
+		url="https://github.com/vladislavkovalskyi/aioowm",
+		license="MIT",
+		packages=setuptools.find_packages(),
+		author_email="v.darknesssb@gmail.com",
+		classifiers=[
+			"Programming Language :: Python :: 3.6",
+			"Programming Language :: Python :: 3.7",
+			"Operating System :: OS Independent",
+		],
+		install_requires=["aiohttp", "pydantic"]
 )
