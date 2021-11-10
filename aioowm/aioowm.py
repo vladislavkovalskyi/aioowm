cb72374f0667407d02eadb50ffdb58e7977af597
vladislavkovalskyi <v.darknesssb@gmail.com>
2021-11-10 10:29:31 +0300

    Обновил aioowm.py Добавил response_handler()

--- aioowm/aioowm.py
+++ aioowm/aioowm.py
@@ -1,35 +1,38 @@
 from typing import Optional
 
 from aioowm.const import const
+from aioowm.handlers.language import language_handler
+from aioowm.handlers.response import response_handler
+from aioowm.handlers.temperature import temperature_handler
 from aioowm.http.http import request
-from aioowm.types.models import Model
-from aioowm.handlers.handlers import language, temperature
+from aioowm.types.model import Model
 
 
 class OWM:
-	def __init__(
-			self,
-			token: str,
-			language: Optional[str] = "en",
-			temperature: Optional[str] = "celsius"
-	):
-		"""
-		:param token: your api-key https://home.openweathermap.org/api_keys
-		:param language: accepts country identifier (en, ru, fr)
-		:param temperature: accepts temperature (celsius, fahrenheit, kelvin)
-		"""
+    def __init__(
+            self,
+            token: str,
+            language: Optional[str] = "en",
+            temperature: Optional[str] = "celsius"
+    ):
+        """
+        :param token: your api-key https://home.openweathermap.org/api_keys
+        :param language: accepts country identifier (en, ru, fr)
+        :param temperature: accepts temperature (celsius, fahrenheit, kelvin)
+        """
 
-		self._token = token
-		self.language = language
-		self.temperature = temperature
+        self._token = token
+        self.language = language
+        self.temperature = temperature
 
-	async def get(self, city: str) -> Model:
-		response = await request(
-				const.weather_api, {
-					"q": city,
-					"lang": await language(self.language),
-					"units": await temperature(self.temperature),
-					"appid": self._token
-				}
-		)
-		return Model(**response)
+    async def get(self, city: str) -> Model:
+        response = await request(
+            const.WEATHER_API, {
+                "q": city,
+                "lang": await language_handler(self.language),
+                "units": await temperature_handler(self.temperature),
+                "appid": self._token
+            }
+        )
+        response = await response_handler(response)
+        return Model(**response)
